cfa89617442494dc46e368bdb9fef4ace2918912
rmn@mbp <rohoehn123@gmail.com>
2023-01-10 14:28:26 +0100

    new evaluations

--- data/summarize_finalFitness.py
+++ data/summarize_finalFitness.py
@@ -14,7 +14,6 @@ FILENAME="best_final_fitness.json"
 
 def getPVal(sampleA, sampleB):
     stats, pval = mwu(sampleA, sampleB)
-    print(pval)
     if pval <= 0.01:
         return f"{pval:.6f}***"
     elif pval <= 0.05:
@@ -25,7 +24,7 @@ def getPVal(sampleA, sampleB):
         return f"{pval:.6f}"
 
 
-csv_string = "Problem,Set,DAE-GP,Pre-Trained_DAE-GP,P_Value\n"
+csv_string = "Problem,Hidden_Layer,Dataset,DAE-GP,Pre-Trained_DAE-GP,P_Value\n"
 
 for problem,path in RESULTS.items():
     d = json.load(open(join(path, FILENAME),"r",encoding="utf-8"))
@@ -38,9 +37,11 @@ for problem,path in RESULTS.items():
     assert d["DAE-GP (train)"] != d["Pre-Trained (train)"]
     assert d["DAE-GP (test)"] != d["Pre-Trained (test)"]
 
+    d["hiddenLayer"]
 
 
-    csv_string += f"{problem},Train,{reg_med_train},{pt_med_train},{getPVal(d['DAE-GP (train)'], d['Pre-Trained (train)'])}\n,Test,{reg_med_test},{pt_med_test},{getPVal(d['DAE-GP (test)'], d['Pre-Trained (test)'])}\n"
+
+    csv_string += f"{d['problem']},{d['hiddenLayer']},Train,{reg_med_train},{pt_med_train},{getPVal(d['DAE-GP (train)'], d['Pre-Trained (train)'])}\n,{d['hiddenLayer']},Test,{reg_med_test},{pt_med_test},{getPVal(d['DAE-GP (test)'], d['Pre-Trained (test)'])}\n"
 
 
 with open("/Users/rmn/github/master_thesis/data/summary_table_final_fit.csv", "w", encoding="utf-8") as f:
