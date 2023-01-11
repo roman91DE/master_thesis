4a7e5469965a57f6cf43a922c520854f63601551
rmn@mbp <rohoehn123@gmail.com>
2023-01-11 11:00:10 +0100

    Wed Jan 11 11:00:10 CET 2023: update-remote.sh

--- data/summarize_csv_files.py
+++ data/summarize_csv_files.py
@@ -1,6 +1,7 @@
 import json
 from scipy.stats import mannwhitneyu as mwu
-from statistics import median, mean
+from scipy.stats import normaltest
+
 from os.path import join
 import numpy as np
 
@@ -12,13 +13,14 @@ RESULTS = {
     #"concrete": "/Users/rmn/github/master_thesis/data/concrete_2hl_FullRun_30gens"
 }
 
+
 FILENAME="best_final_fitness.json"
 
 def getPVal(sampleA, sampleB):
     stats, pval = mwu(sampleA, sampleB)
-    assert len(sampleA) > 6 and len(sampleB) > 6
 
-    print(pval)
+    assert len(sampleA) > 6 and len(sampleB) >= 8
+
     if pval <= 0.01:
         return f"{pval:.4f}***"
     elif pval <= 0.05:
@@ -29,12 +31,26 @@ def getPVal(sampleA, sampleB):
         return f"{pval:.4f}"
 
 
+# Fitness
+# ---
+
 # summarize by mean
 csv_string = "Problem,Hidden_Layer,Dataset,DAE-GP,Pre-Trained_DAE-GP,P_Value\n"
 
 for problem,path in RESULTS.items():
 
     d = json.load(open(join(path, FILENAME),"r",encoding="utf-8"))
+
+    # test for normal distribution ?
+    # ----
+    # reg_train = d["DAE-GP (train)"]
+    # pt_train = d["Pre-Trained (train)"]
+    # reg_test = d["DAE-GP (test)"]
+    # pt_test = d["Pre-Trained (test)"]
+
+    # for vals in (reg_train, pt_train, reg_test, pt_test):
+    #     s, p = normaltest(vals)
+    #     print("p_val:", p)
 
     reg_mean_train = np.mean(d["DAE-GP (train)"])
     pt_mean_train = np.mean(d["Pre-Trained (train)"])
@@ -73,3 +89,29 @@ for problem,path in RESULTS.items():
 
 with open("/Users/rmn/github/master_thesis/data/summary_table_final_fit_median.csv", "w", encoding="utf-8") as f:
     f.write(csv_string)
+
+
+
+FILENAME_SIZE = "size_best_solution.json"
+
+# Solution Size
+# ---
+
+# summarize by mean
+csv_string = "Problem,Hidden_Layer,DAE-GP,Pre-Trained_DAE-GP,P_Value\n"
+
+for problem,path in RESULTS.items():
+
+    d = json.load(open(join(path, FILENAME_SIZE),"r",encoding="utf-8"))
+
+    
+    reg_mean = np.mean(d["DAE-GP"])
+    pt_mean = np.mean(d["Pre-Trained"])
+
+
+    csv_string += f"{d['problem']},{d['hiddenLayer']},{reg_mean},{pt_mean},{getPVal(d['DAE-GP'], d['Pre-Trained'])}\n"
+
+
+with open("/Users/rmn/github/master_thesis/data/summary_table_best_size_mean.csv", "w", encoding="utf-8") as f:
+    f.write(csv_string)
+
